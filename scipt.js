a214e20991cd02825cbf6c7c0a6d65e1e2905dc9
spelledbymedusa <tessathejedi@gmail.com>
2025-12-18 10:11:30 +0100

    Remove filter and sort button event listeners

--- scipt.js
+++ scipt.js
@@ -1,7 +1,1 @@
-document.getElementById("filterBtn").addEventListener("click", () => {
-  alert("Filter-Funktion kommt später 🙂");
-});
 
-document.getElementById("sortBtn").addEventListener("click", () => {
-  alert("Sortier-Funktion kommt später 🙂");
-});
